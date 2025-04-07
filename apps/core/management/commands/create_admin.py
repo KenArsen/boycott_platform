4810a604640463703520dce6000cb684edc33166
KenArsen <kenzhegulov.kgz@gmail.com>
2025-04-07 19:42:50 +0600

    Изменен имя контейнера api->web и добавлен команда для создание супер пользователя

---
+++ apps/core/management/commands/create_admin.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from apps.account.models import User
+
+
+class Command(BaseCommand):
+    help = "Creates a default superuser"
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(email="admin@gmail.com").exists():
+            User.objects.create_superuser(
+                email="admin@gmail.com",
+                password="admin",
+                first_name="Admin",
+                last_name="Admin",
+                phone_number="0 (XXX) XX XX XX",
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created: admin@gmail.com / admin"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))

169abc284cb346ff886c3e31c0edb727bee12453
KenArsen <kenzhegulov.kgz@gmail.com>
2025-04-10 10:55:56 +0600

    Добавление переменной окружения DJANGO_ENV для управления настройками окружения

--- boycott_platform/wsgi.py
+++ boycott_platform/wsgi.py
@@ -2,6 +2,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boycott_platform.settings")
+from boycott_platform.settings.environment import env
+
+environment = env.str("DJANGO_ENV", default="dev")
+
+assert environment in ["dev", "prod"], f"Неверное значение DJANGO_ENV: {environment}. Ожидается 'dev' или 'prod'."
+
+print(f"Запуск с окружением: {environment}")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"boycott_platform.settings.{environment}")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,10 +3,19 @@
 import os
 import sys
 
+from boycott_platform.settings.environment import env
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boycott_platform.settings.dev")
+    environment = env.str("DJANGO_ENV", default="dev")
+
+    assert environment in ["dev", "prod"], f"Неверное значение DJANGO_ENV: {environment}. Ожидается 'dev' или 'prod'."
+
+    print(f"Запуск с окружением: {environment}")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"boycott_platform.settings.{environment}")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

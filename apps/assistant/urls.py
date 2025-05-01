5abbe4b991ae59bfa01655d97ec1306f531e3fdc
KenArsen <kenzhegulov.kgz@gmail.com>
2025-05-01 13:52:42 +0600

    Шаблон

--- apps/assistant/urls.py
+++ apps/assistant/urls.py
@@ -1,12 +1,11 @@
 # Добавьте эти строки в urls.py
 from django.urls import path
 
-from apps.assistant.views import chat, simple
+from apps.assistant.views.boycott_asistant import chatbot_ui, search_product_api
 
 app_name = "assistant"
 
 urlpatterns = [
-    # Другие URL маршруты
-    path("api/chat/", chat.chat_api, name="chat_api"),
-    path("simple/", simple.chat_api, name="chat_api"),
+    path("", chatbot_ui, name="chatbot_ui"),
+    path("search-product/", search_product_api, name="search_product_api"),
 ]
---
+++ apps/assistant/views/boycott_asistant.py
@@ -0,0 +1,42 @@
+import json
+
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
+
+from apps.product.models import Product
+
+
+def chatbot_ui(request):
+    return render(request, "product_assistant.html")
+
+
+@csrf_exempt
+def search_product_api(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            query = data.get("query", "").strip().lower()
+
+            product = (
+                Product.objects.select_related("category", "boycott_reason")
+                .prefetch_related("alternative_products")
+                .filter(name__icontains=query)
+                .first()
+            )
+            print(product)
+
+            if not product:
+                return JsonResponse(
+                    {
+                        "success": False,
+                        "html": f'К сожалению, я не нашел информации о продукте "{query}". '
+                        f"Попробуйте ввести другое название.",
+                    }
+                )
+
+            html = render_to_string("product_card.html", {"product": product})
+            return JsonResponse({"success": True, "html": html})
+        except Exception as e:
+            return JsonResponse({"success": False, "html": f"Ошибка: {str(e)}"})
